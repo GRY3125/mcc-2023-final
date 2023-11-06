0dc0369f9984ed3021ec01aeff8834fa36f6d59a
GRY3125 <ruyinggan@gmail.com>
2023-11-06 19:22:28 +0800

    p1

---
+++ Problem 1 - Collatz/p1code.py
@@ -0,0 +1,19 @@
+n=733
+k=559
+
+fp = open("Problem 1 - Collatz/p1input.txt")
+for line in fp.readlines():
+    nlist=line
+fp.close() 
+
+nlist=nlist.split()
+sum=0
+for x in range(n): # for every number
+    nlist[x]=int(nlist[x])
+    for y in range(k): 
+        if nlist[x]%2==0: 
+            nlist[x]=nlist[x]/2
+        else: 
+            nlist[x]=3*(nlist[x])+1
+    sum+=nlist[x]
+print(sum)
